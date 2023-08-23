a7d5d62442d438e3cd2de17191263f8ba5eaf516
harnihal <106397073+harnihal@users.noreply.github.com>
2023-08-23 08:24:30 +0530

    Delete twttr.py

--- twttr.py
+++
@@ -1,9 +0,0 @@
-x = input("Input: ")
-
-print("Output: ", end="")
-
-for vowels in x:
-    if not vowels in ['a', 'A', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']:
-        print(vowels, end="")
-
-print()
